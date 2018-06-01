8e15f13116d179e9102f77e18857619bf1267801
Rick Strahl <rstrahl@west-wind.com>
2018-06-01 02:25:44 -0700

    Rebuild with Edward Brey's rewritten CLR4 hosting code.

--- ClrHost/ClrHost.cpp
+++ ClrHost/ClrHost.cpp
@@ -1,5 +1,6 @@
 #include <atlbase.h>
 #include <mscoree.h>
+#include <metahost.h> 
 
 #import "mscorlib.tlb" raw_interfaces_only	no_smart_pointers high_property_prefixes("_get","_put","_putref") 
 
@@ -13,82 +14,94 @@ CComPtr<ICorRuntimeHost>	spRuntimeHost = NULL;
 CComPtr<_AppDomain>			spDefAppDomain = NULL;
 CComPtr<_Object>             spDefObject = NULL;
 
-BSTR ClrVersion = NULL;
+// .NET Framework 4
+CComPtr<ICLRMetaHost> pMetaHost;
+CComPtr<ICLRRuntimeInfo> pRuntimeInfo;
+
+CComBSTR ClrVersion;
 
 DWORD WINAPI SetClrVersion(char *version)
 {
-	ClrVersion = CComBSTR(version);
+	ClrVersion = version;
 	return 1;
 }
 
-/// Assigns an error message
-DWORD WINAPI SetError(HRESULT hr, char *ErrorMessage)
+class HresultException
 {
-	if (ErrorMessage)
-	{ 			
-		int len= strlen(ErrorMessage);		
-		LoadStringRC(hr & 0xffff,(LPWSTR)ErrorMessage,len/2,0);																
-		sprintf((char *)ErrorMessage,"%ws",ErrorMessage);
-				
-		return strlen(ErrorMessage);
+	char const* const error;
+	HRESULT const hr;
+
+public:
+	HresultException(char const* error) : error(error), hr(0) {}
+	HresultException(HRESULT hr) : error(nullptr), hr(hr) {}
+
+	DWORD GetMessage(char* errorMessage)
+	{
+		auto outputSize = strlen(errorMessage);
+		if (error) {
+			strcpy_s(errorMessage, outputSize, error);
+		} else {
+#pragma warning(push)
+#pragma warning(disable: 4996) // LoadStringRC is deprecated, but no alternative is suggested.
+			LoadStringRC(hr & 0xffff, (LPWSTR)errorMessage, outputSize / 2, 0);
+#pragma warning(pop)
+			sprintf_s((char *)errorMessage, outputSize, "%ws", (LPWSTR)errorMessage);
+		}
+		return strlen(errorMessage);
 	}
-	 
-	strcpy(ErrorMessage,"");
-	return 0;
+};
+
+void VerifyHresult(HRESULT hr)
+{
+	if (FAILED(hr))
+		throw HresultException(hr);
 }
 
 /// Starts up the CLR and creates a Default AppDomain
-DWORD WINAPI ClrLoad(char *ErrorMessage, DWORD *dwErrorSize)
+void WINAPI ClrLoadLegacyVersion2()
 {
-	if (spDefAppDomain)
-		return 1;
-
-	
+#pragma warning(push)
+#pragma warning(disable: 4996) // CorBindToRuntimeEx is deprecated, but we need to use it to start CLR version 2.
 	//Retrieve a pointer to the ICorRuntimeHost interface
-	HRESULT hr = CorBindToRuntimeEx(
-					ClrVersion,	//Retrieve latest version by default
-					L"wks",	//Request a WorkStation build of the CLR
-					STARTUP_LOADER_OPTIMIZATION_MULTI_DOMAIN | STARTUP_CONCURRENT_GC, 
-					CLSID_CorRuntimeHost,
-					IID_ICorRuntimeHost,
-					(void**)&spRuntimeHost
-					);
-
-	if (FAILED(hr)) 
-	{
-		*dwErrorSize = SetError(hr,ErrorMessage);	
-		return hr;
-	}
+	VerifyHresult(CorBindToRuntimeEx(
+		ClrVersion,	//Retrieve latest version by default
+		L"wks",	//Request a WorkStation build of the CLR
+		STARTUP_LOADER_OPTIMIZATION_MULTI_DOMAIN | STARTUP_CONCURRENT_GC,
+		CLSID_CorRuntimeHost,
+		IID_ICorRuntimeHost,
+		(void**)&spRuntimeHost
+	));
+#pragma warning(pop)
 
 	//Start the CLR
-	hr = spRuntimeHost->Start();
-
-	if (FAILED(hr))
-		return hr;
+	VerifyHresult(spRuntimeHost->Start());
 
 	CComPtr<IUnknown> pUnk;
-
-	//Retrieve the IUnknown default AppDomain
-	//hr = spRuntimeHost->GetDefaultDomain(&pUnk);
-	//if (FAILED(hr)) 
-	//	return hr;
-
-	
 	WCHAR domainId[50];
-	swprintf(domainId,L"%s_%i",L"wwDotNetBridge",GetTickCount());
-	hr = spRuntimeHost->CreateDomain(domainId,NULL,&pUnk);	
-
-	//spRuntimeHost->CreateDomainSetup(&pUnk);
-	//
-
-	hr = pUnk->QueryInterface(&spDefAppDomain.p);
-	if (FAILED(hr)) 
-		return hr;
-	
-	return 1;
+	swprintf(domainId, 50, L"%s_%i", L"wwDotNetBridge", GetTickCount());
+	VerifyHresult(spRuntimeHost->CreateDomain(domainId, NULL, &pUnk));
+	VerifyHresult(pUnk->QueryInterface(&spDefAppDomain.p));
 }
 
+void WINAPI ClrLoad()
+{
+	if (ClrVersion.Length() >= 2 && ClrVersion[0] == 'v' && ClrVersion[1] == '2') {
+		ClrLoadLegacyVersion2();
+		return;
+	}
 
+	// Tutorial on how this works: https://code.msdn.microsoft.com/windowsdesktop/CppHostCLR-e6581ee0
+	VerifyHresult(CLRCreateInstance(CLSID_CLRMetaHost, IID_PPV_ARGS(&pMetaHost)));
+	VerifyHresult(pMetaHost->GetRuntime(ClrVersion, IID_PPV_ARGS(&pRuntimeInfo)));
+	BOOL fLoadable;
+	VerifyHresult(pRuntimeInfo->IsLoadable(&fLoadable));
+	if (!fLoadable) throw HresultException("CLR is not loadable.");
+	VerifyHresult(pRuntimeInfo->GetInterface(CLSID_CorRuntimeHost, IID_PPV_ARGS(&spRuntimeHost)));
+	VerifyHresult(spRuntimeHost->Start());
+	CComPtr<IUnknown> spAppDomainThunk;
+	VerifyHresult(spRuntimeHost->GetDefaultDomain(&spAppDomainThunk));
+	VerifyHresult(spAppDomainThunk->QueryInterface(IID_PPV_ARGS(&spDefAppDomain)));
+}
 
 // *** Unloads the CLR from the process
 DWORD WINAPI ClrUnload()
@@ -99,95 +112,46 @@ DWORD WINAPI ClrUnload()
 		spDefAppDomain.Release();
 		spDefAppDomain = NULL;
 
-		spRuntimeHost->Stop();		
+		spRuntimeHost->Stop();
 		spRuntimeHost.Release();
 		spRuntimeHost = NULL;
 	}
-	
+
 	return 1;
 }
 
 
 // *** Creates an instance by Name (ie. local path assembly without extension or GAC'd FullName of 
 //      any signed assemblies.
-DWORD WINAPI ClrCreateInstance( char *AssemblyName, char *className, char *ErrorMessage, DWORD *dwErrorSize)
+IDispatch* WINAPI ClrCreateInstance(char *AssemblyName, char *className, char *ErrorMessage, DWORD *dwErrorSize)
 {
-	CComPtr<_ObjectHandle> spObjectHandle;
-	
-	if (!spDefAppDomain)
-	{		
-		if (ClrLoad(ErrorMessage,dwErrorSize) != 1)
-			return -1;
+	try {
+		if (!spDefAppDomain)
+			ClrLoad();
+		CComPtr<_ObjectHandle> spObjectHandle;
+		VerifyHresult(spDefAppDomain->CreateInstance(_bstr_t(AssemblyName), _bstr_t(className), &spObjectHandle));
+		CComVariant VntUnwrapped;
+		VerifyHresult(spObjectHandle->Unwrap(&VntUnwrapped));
+		return VntUnwrapped.pdispVal;
+	} catch (HresultException ex) {
+		*dwErrorSize = ex.GetMessage(ErrorMessage);
+		return NULL;
 	}
-
-	DWORD hr;
-
-	//Creates an instance of the type specified in the Assembly
-	hr = spDefAppDomain->CreateInstance(
-										_bstr_t(AssemblyName), 
-										_bstr_t(className),
-										&spObjectHandle
-	);
-
-
-	
-	*dwErrorSize = 0;
-
-	if (FAILED(hr)) 
-	{		
-		*dwErrorSize = SetError(hr,ErrorMessage);
-		return -1;
-	}
-
-	CComVariant VntUnwrapped;
-	hr = spObjectHandle->Unwrap(&VntUnwrapped);
-	if (FAILED(hr))
-		return -1;
-
-
-	CComPtr<IDispatch> pDisp;
-	pDisp = VntUnwrapped.pdispVal;	
-	
-	return (DWORD) pDisp.p;
 }
 
-/// *** Creates an instance of a class from an assembly referenced through it's disk path
-DWORD WINAPI ClrCreateInstanceFrom( char *AssemblyFileName, char *className, char *ErrorMessage, DWORD *dwErrorSize)
+/// *** Creates an instance of a class from an assembly referenced through its disk path
+IDispatch* WINAPI ClrCreateInstanceFrom(char *AssemblyFileName, char *className, char *ErrorMessage, DWORD *dwErrorSize)
 {
-	CComPtr<_ObjectHandle> spObjectHandle;
-	
-	if (!spDefAppDomain)
-	{		
-		if (ClrLoad(ErrorMessage,dwErrorSize) != 1)
-			return -1;
+	try {
+		if (!spDefAppDomain)
+			ClrLoad();
+		CComPtr<_ObjectHandle> spObjectHandle;
+		VerifyHresult(spDefAppDomain->CreateInstanceFrom(_bstr_t(AssemblyFileName), _bstr_t(className), &spObjectHandle));
+		CComVariant VntUnwrapped;
+		VerifyHresult(spObjectHandle->Unwrap(&VntUnwrapped));
+		return VntUnwrapped.pdispVal;
+	} catch (HresultException ex) {
+		*dwErrorSize = ex.GetMessage(ErrorMessage);
+		return NULL;
 	}
-
-	DWORD hr;
-
-	//Creates an instance of the type specified in the Assembly
-	hr = spDefAppDomain->CreateInstanceFrom(		
-									_bstr_t(AssemblyFileName), 
-									_bstr_t(className),
-									&spObjectHandle
-	);	
-
-	*dwErrorSize = 0;
-
-	if (FAILED(hr)) 
-	{		
-		*dwErrorSize = SetError(hr,ErrorMessage);
-		return -1;
-	}
-
-	CComVariant VntUnwrapped;
-	hr = spObjectHandle->Unwrap(&VntUnwrapped);
-	if (FAILED(hr))
-		return -1;
-
-	CComPtr<IDispatch> pDisp;
-	pDisp = VntUnwrapped.pdispVal;
-	
-	// *** pass the raw COM pointer back
-	return (DWORD) pDisp.p;
 }
-
