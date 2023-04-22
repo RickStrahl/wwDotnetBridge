021cb49a8c01a0b04e9496f8b6404b6bcdae8a54
rstrahl <rstrahl@west-wind.com>
2023-04-21 14:37:29 -1000

    Sign binaries

--- Distribution/sign.ps1
+++ Distribution/sign.ps1
@@ -1,1 +1,2 @@
-& "c:\utl\signtool.exe" sign /v /n "West Wind Technologies" /sm /s MY /tr "http://timestamp.digicert.com" /td SHA256 /fd SHA256 ".\wwdotnetbridge.dll"
+& "~\Dropbox\utl\signtool.exe" sign /v /n "West Wind Technologies" /sm /s MY /tr "http://timestamp.digicert.com" /td SHA256 /fd SHA256 ".\wwdotnetbridge.dll"
+& "~\Dropbox\utl\signtool.exe" sign /v /n "West Wind Technologies" /sm /s MY /tr "http://timestamp.digicert.com" /td SHA256 /fd SHA256 ".\clrhost.dll"
